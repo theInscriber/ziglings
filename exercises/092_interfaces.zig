140f44eb0e29c90721b3b932e1f1761696f4a1a5
Samuel Yute <samiyutez@gmail.com>
2024-04-10 17:05:46 +0200

    Solved another 7

--- exercises/092_interfaces.zig
+++ exercises/092_interfaces.zig
@@ -104,9 +104,9 @@ pub fn main() !void {
 
     std.debug.print("Daily Insect Report:\n", .{});
     for (my_insects) |insect| {
+       insect.print();
         // Almost done! We want to print() each insect with a
         // single method call here.
-        ???
     }
 }
 
